917a24e9242465940a8815da1baf89123a8a15d2
Hugo <hugopozzoli@gmail.com>
2016-11-21 19:12:50 +0100

    Updated projects @ mon. 21 nov. 2016

---
+++ la-vie-a-sciencescom/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.jour').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('clicked on ' + $(e.target).attr('id'));
+
+    $('#popup').addClass('active');
+  });
+
+  $('#popup-close, #popup-backdrop').on('click', function() {
+    $('#popup').removeClass('active');
+  })
+});
